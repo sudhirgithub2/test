574f31d213a413310b8d6f902d1b99666b12defd
sudhir <sk9205170513@gmail.com>
2019-05-15 23:44:05 +0530

    successfully commited

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+		
+		<input type="file" name="profile[]" multiple>
+		<br><br><input type="submit" name="submit">
+
+	</form>
+
+</body>
+</html>
